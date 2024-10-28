2965cc166be6a46577fbc7f6d0eb5998faf5b523
jjdubski <jjwaksmanski@gmail.com>
2024-10-28 18:27:12 -0500

    added vite

---
+++ vite/src/App.jsx
@@ -0,0 +1,60 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './pages/index';
+import About from './pages/about/about';
+import Events from './pages/events/events';
+import Projects from './pages/projects/projects';
+import Eboard from './pages/eboard/eboard';
+import "./App.css"
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/eboard" element={<Eboard />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ vite/src/pages/index/Index.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+import Navbar from '../../components/navbar/Navbar'
+import Welcome from '../../components/welcome/Welcome';
+import "./index.css"
+
+
+const Index = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className='index'>
+      <Welcome/>
+    </div>
+    </>
+  )
+}
+
+export default Index;
