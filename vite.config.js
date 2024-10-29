327ca910831e9e428fec61aabe06f4ffc5804649
jjdubski <jjwaksmanski@gmail.com>
2024-10-28 20:05:06 -0500

    vite config and maybe path fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 // import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // '@' now points to the 'src' folder
+    },
+  },
 })
