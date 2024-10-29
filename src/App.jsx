c0cf69e246d0aede4a66215af93f93f468637b53
jjdubski <jjwaksmanski@gmail.com>
2024-10-28 19:11:54 -0500

    vercel dir fix attempt #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/index/Index';
 import About from './pages/about/About';
