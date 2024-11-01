fb07f8b228468b3cc9070f3faffe45032bf319ac
jjdubski <jjwaksmanski@gmail.com>
2024-11-01 00:31:28 -0500

    hover effect

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,10 +16,10 @@ return (
             <div className="copyright">© 2024 University of DePaul Computer Science Society</div>
         </div>
         <div className="footer-right">
-            <Link to="https://www.instagram.com/depaulcss/"><div className="instagram"><img src={ig} className="igLogo"></img></div></Link>
-            <Link to="https://discord.gg/2WN7ZyDV7j"><div className="discord"><img src={discord} className="discordLogo"></img></div></Link>
-            <Link to="https://www.facebook.com/groups/cssdepaul/"><div className="facebook"><img src={fb} className="fbLogo"></img></div></Link>
-            <Link to="mailto:cssdepaul@gmail.com"><div className="email"><img src={mail} className="emailLogo"></img></div></Link>
+            <Link to="https://www.instagram.com/depaulcss/"><div className="social"><img src={ig} className="igLogo"></img></div></Link>
+            <Link to="https://discord.gg/2WN7ZyDV7j"><div className="social"><img src={discord} className="discordLogo"></img></div></Link>
+            <Link to="https://www.facebook.com/groups/cssdepaul/"><div className="social"><img src={fb} className="fbLogo"></img></div></Link>
+            <Link to="mailto:cssdepaul@gmail.com"><div className="social"><img src={mail} className="emailLogo"></img></div></Link>
         </div>
     </div>
   )
