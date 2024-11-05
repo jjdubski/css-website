7ec3f1853eae0360f125b324e7479784eba683d8
jjdubski <jjwaksmanski@gmail.com>
2024-11-05 09:27:43 -0600

    msg-cont fix

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -15,18 +15,18 @@ const Welcome = () => {
             <p className="msg">The Computer Science Society (CSS) at DePaul University is a dynamic, student-run organization 
                 dedicated to fostering a supportive and inclusive community for students passionate about technology, innovation, 
                 and computing. Our mission is to advocate for and promote the field of computer science across the university, 
-                while also providing a welcoming environment for students from all academic backgrounds—whether you're pursuing a 
+                while also providing a welcoming environment for students from all academic backgrounds—whether you&apos;re pursuing a 
                 degree in computer science or simply have an interest in technology.
             </p>
             <p className="msg">At CSS, we believe that collaboration and knowledge-sharing are the keys to success in the tech industry. 
                 We host a variety of events, workshops, and panels that encourage networking, skill development, and professional growth. 
-                From coding bootcamps and hackathons to industry guest speaker sessions and career-building workshops, there’s something 
+                From coding bootcamps and hackathons to industry guest speaker sessions and career-building workshops, there&apos;s something 
                 for everyone, regardless of experience level. Our club provides hands-on opportunities to work on real-world projects, 
                 preparing students for future careers in technology while connecting them with industry professionals.
             </p>
             <p className="msg">We are also passionate about promoting diversity in tech. By partnering with other student organizations 
                 and departments, we aim to create a space that uplifts underrepresented voices and advocates for equity and inclusion 
-                within the computing field. Whether you're a seasoned coder or just curious about technology, CSS is a place where you 
+                within the computing field. Whether you&apos;re a seasoned coder or just curious about technology, CSS is a place where you 
                 can learn, grow, and connect with like-minded individuals.
             </p>
             <p className="msg">Join us to explore new technologies, develop skills, and be part of a thriving community dedicated to 
