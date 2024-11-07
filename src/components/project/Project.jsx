db2b491bf352849055d63c318ac983b436820f14
jjdubski <101855602+jjdubski@users.noreply.github.com>
2024-11-06 20:03:01 -0600

    projects tiles fix

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,15 +1,15 @@
-import "./project.css"
 import chevron from "/images/chevron-right.svg"
+import "./project.css"
 
 const Project = () => { 
 return (
     <div className='tile-cont'>
         <h2 className="subtitle">Project Title</h2>
         <div className="tile-body">
-            <p className="project-desc">Project Description</p>
-            <p className="info" id="project-status">Project Status</p>
-            <p className="learn-more">Learn more<img src={chevron} alt=">" height={10}/></p>
+            <p className="info project-status">Project Status</p>
+            <p className="info project-desc">Project Description</p>
         </div>
+        <p className="learn-more">Learn more<img src={chevron} alt=">" height={10}/></p>
     </div>
   )
 }
